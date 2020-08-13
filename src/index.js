fdd44b86b21804dad843c1f77845cd43f7da4871
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-13 15:37:02 +0200

    clean index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-// 👉 Importing React libs from node_modules folder
 import React from 'react'
 import { render } from 'react-dom'
 
-// 👉 Importing a React component from another file
 import App from './components/App'
 
-// 👉 Importing our styles
 import './styles.css'
 
 render(
