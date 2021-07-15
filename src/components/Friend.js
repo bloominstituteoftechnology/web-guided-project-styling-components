dcbf9c7ef39614c05d930e3762cb93fb5a215262
Christopher Atoki <christopheratoki@Christophers-MacBook-Pro.local>
2021-07-15 13:26:21 -0400

    wip

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -5,6 +5,15 @@ const StyledFriend = styled.div`
   width: 60%;
   display: flex;
   justify-content: space-between;
+  padding: 8px;
+  border-bottom: 2px solid white;
+
+  background-color: ${props => props.theme.primaryColor};
+  color: ${props => props.theme.white};
+
+  @media ${props => props.theme.breakpointMobile} {
+    width: initial;
+  }
 `
 
 
