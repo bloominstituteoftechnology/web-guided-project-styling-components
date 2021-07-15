b739ed4692a09a82e75efd329424840b08b102e1
Christopher Atoki <christopheratoki@Christophers-MacBook-Pro.local>
2021-07-15 12:49:47 -0400

    wip

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ worker.start()
 
 import React from 'react'
 import { render } from 'react-dom'
+import { ThemeProvider } from 'styled-components'
 
 import App from './components/App'
+import themeObj from './theme'
 
 import './styles.css'
 
 render(
-  <App />,
+  <ThemeProvider theme={themeObj}>
+    <App />
+  </ThemeProvider>,
   document.querySelector('#root')
 )
