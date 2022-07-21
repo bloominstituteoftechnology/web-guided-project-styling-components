b83aade5fe599938cebdeabd4e4d0b39d250aa90
Casey Harding <casey.r.harding@gmail.com>
2022-07-21 13:22:12 -0400

    Final solution in place.

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import { BASE_URL, API_KEY } from '../constants'
 import axios from 'axios'
+import styled, { keyframes } from "styled-components";
+
+const kf = keyframes`
+  100% {
+    opacity: 1;
+    transform: scale(1) rotateZ(0);
+  }
+`
+
+const StyledDetails = styled.div`
+  opacity: 0;
+  transform: scale(2) rotateZ(180deg);
+  animation: ${kf} 0.5s ease-in-out forwards;
+
+  p {
+    color: ${props => props.theme.tertiaryColor};
+  }
+
+  h2 {
+    color: ${props => props.theme.primaryColor};
+  }
+`
 
 export default function Details(props) {
   const { friendId, close } = props
@@ -13,7 +35,7 @@ export default function Details(props) {
   }, [friendId])
 
   return (
-    <div className='container'>
+    <StyledDetails className='container'>
       <h2>Details:</h2>
       {
         details &&
@@ -29,6 +51,6 @@ export default function Details(props) {
         </>
       }
       <button onClick={close}>Close</button>
-    </div>
+    </StyledDetails>
   )
 }
--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 /**
  * .friend {
@@ -8,6 +8,16 @@ import styled from "styled-components";
       justify-content: space-between;
     }
  */
+
+const kf = keyframes`
+    50% {
+      transform: scale(0.8);
+    }
+    100% {
+      opacity: 1;
+      transform: scale(1);
+    }
+`
 
 const StyledFriend = styled.div`
     width: 60%;
@@ -31,6 +41,17 @@ const StyledFriend = styled.div`
     &::before {
       content: "${pr => pr.besty ? '💚' : '😀'}";
     }
+
+    button {
+      background-color: ${pr => pr.theme.tertiaryColor};
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
+
+    transform: scale(2);
+    opacity: 0;
+    animation: ${kf} 0.3s ease-in-out forwards;
 `
 
 export default function Friend({ info, action, besty }) {
